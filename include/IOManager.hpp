f88466b76d3b0243719ec8b6a52e6529dad0ec5a
olaazeez <141501427+Olaalx@users.noreply.github.com>
2025-05-01 03:50:18 +0300

    Update IOManager.hpp

--- include/IOManager.hpp
+++ include/IOManager.hpp
@@ -1,7 +1,23 @@
-// File: include/IOManager.hpp
 #ifndef IOMANAGER_HPP
 #define IOMANAGER_HPP
+#include "Graph.hpp"
+#include <string>
+#include <fstream> // For file operations
+#include <sstream> // For string stream operations
+#include <iostream> 
 
-// File save/load logic here
+using namespace std;
+
+// File save/load logic here .
+
+class IOManager {
+
+	IOManager();
+public:
+
+	void saveGraph(const string& filename, const Graph& graph);
+	void loadGraph(const string& filename, Graph& graph);
+
+};
 
 #endif
